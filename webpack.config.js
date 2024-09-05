a0e81fa30efc72bb88415cb1a9d5618ad9cdc730
Koda Lee <kodaallen123@gmail.com>
2024-09-05 14:41:17 -0700

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -8,9 +8,9 @@ if (!Encore.isRuntimeEnvironmentConfigured()) {
 
 Encore
     // directory where compiled assets will be stored
-    .setOutputPath('build/')
+    .setOutputPath('public/build/')
     // public path used by the web server to access the output path
-    .setPublicPath('/build')
+    .setPublicPath('/public/build')
     // only needed for CDN's or subdirectory deploy
     //.setManifestKeyPrefix('build/')
 
